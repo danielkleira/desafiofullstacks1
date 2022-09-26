bac7d8b899d7365a13c69fe2d7b185c8ad8fe539
danielkleira <daniel_leira@hotmail.com>
2022-09-26 19:54:05 -0300

    heroku configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,14 +14,18 @@ export const AppDataSource =
       })
     : new DataSource({
         type: "postgres",
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PWD,
-        database: process.env.POSTGRES_DB,
+        host: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production" ?
+            { rejectUnauthorized: false}
+            : false,
         synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        logging: true,
+        entities: process.env.NODE_ENV === "production"
+        ? ["dist/entities/*.js"]
+        : ["src/entities/*.ts"],
+    migrations: process.env.NODE_ENV === "production"
+        ? ["dist/migrations/*.js"]
+        : ["src/migrations/*.ts"],
       });
 
       AppDataSource.initialize()
