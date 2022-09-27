602278ef705cdb006160f527e8c7ed9bb219e5ea
danielkleira <daniel_leira@hotmail.com>
2022-09-26 21:11:37 -0300

    heroku config

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import { routes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import { Request, Response } from "express";
+var cors = require('cors')
+
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/", routes);
